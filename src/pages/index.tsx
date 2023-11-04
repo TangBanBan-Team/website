50bd78caa91a5f72da09fbeac0cc37d0a32ff593
MengXi <me@zhunote.cn>
2023-11-04 08:01:39 +0800

    Update index page

---
+++ src/pages/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
+import styles from './index.module.css';
+
+function HomepageHeader() {
+    const {siteConfig} = useDocusaurusContext();
+    return (
+        <header className={clsx('hero hero--primary', styles.heroBanner)}>
+            <div className="container">
+                <h1 className="hero__title">{siteConfig.title}</h1>
+                <p className="hero__subtitle">{siteConfig.tagline}</p>
+                <div className={styles.buttons}>
+                    <Link
+                        className="button button--secondary button--lg"
+                        to="/docs/intro">
+                        "银发族"网络使用指南
+                    </Link>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default function Home(): JSX.Element {
+    const {siteConfig} = useDocusaurusContext();
+    return (
+        <Layout
+            title={`首页`}
+            description="DailyNotes Team">
+            <div>
+                <div className={styles.HomepageTips}>
+                    此站点为Dev版本，仍在制作当中，不代表最终成品
+                </div>
+            </div>
+            <HomepageHeader />
+            <main>
+                <HomepageFeatures />
+            </main>
+        </Layout>
+    );
+}
